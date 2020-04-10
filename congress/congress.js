9644079bc4982153104e4be432b9924d2ca66248
BobbyWasabi <60754155+BobbyWasabi@users.noreply.github.com>
2020-04-10 17:45:54 -0600

    added simplified Senators to congress

--- congress/congress.js
+++ congress/congress.js
@@ -1,59 +1,64 @@
-import { senators } from '../Starwars/data/senators.js'
-console.log(senators)
-const senatorDiv = document.querySelector('senators')
+//import { senators } from '../Starwars/data/senators.js'
+//console.log(senators)
+const senatorDiv = document.querySelector('.senators')
 
 //function getSimplifiedSen (senatorArray) {
     //return senatorArray.map(senator => {
-function populateSenatorDiv() {
-    senators.forEach(senator => {
-        let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
-
-        let senFigure = document.createElement('figure')
-        let figImg = document.createElement('img')
-        let figCaption = document.createElement('figcaption')
-
-        figImg.src = `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`
-        figCaption.textContent = `${senator.first_name}${middleName}${senator.last_name}`
-
-    //   figImg.src = senator.imgURL
-    //   figCaption.textContent = senator.name
-        
-        senFigure.appendChild(figImg)
-        senFigure.appendChild(figCaption)
-        senatorDiv.appendChild(senFigure)
+    function getSimplifiedSenators(senatorArray) {
+        return senatorArray.map(senator => {
+            let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
+            return {
+                id: senator.id,
+                name: `${senator.first_name}${middleName}${senator.last_name}`,
+                imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
+                seniority: parseInt(senator.seniority, 10),
+                missedVotesPct: senator.missed_votes_pct,
+                party: senator.party,
+                loyaltyPct: senator.votes_with_party_pct
+        }
         })
-    main.appendChild(senatorDiv)
-}
+    }
+    function populateSenatorDiv(simpleSenators) {
+        console.log(simpleSenators)
+        simpleSenators.forEach(senator => {
+            let senFigure = document.createElement('figure')
+            let figImg = document.createElement('img')
+            let figCaption = document.createElement('figcaption')
+                
+            figImg.src = senator.imgURL
+            figCaption.textContent = senator.name
+    
+            senFigure.appendChild(figImg)
+            senFigure.appendChild(figCaption)
+            senatorDiv.appendChild(senFigure)
+        })
+    }
 //    return {
 //        id: senator.id,
 //        name: `${senator.first_name}${$middleName}${senator.last_name} `,
-        
-        //seniority : parseInt(senator.seniority, 10
-   //}
-//})
 
-function populateSenatorDiv(justSenatorNames) {justSenatorNames.forEach(senator => {
-    console.log(justSenatorNames)
-    justSenatorNames.forEach(senator => {
-
-
-})
-
-})
 
 const filterSenators = (prop, value) => {
     return senators.filter(senator => {
         return senator[prop] === value
-})
+    })
 }
-
 const republicans = filterSenators('party', 'R')
+const democrats = filterSenators('party', 'D')
 
-const mostSeniority = getSimplifiedSenators(republicans).reduce(
-    (acc, senator) => {
-        return (acc.seniority || 0) > senior.seniority ? acc : senator
+const mostSeniority = getSimplifiedSenators(senators).reduce((acc, senator) => acc.seniority > senator.seniority ? acc : senator)
+
+const missedVotes = getSimplifiedSenators(senators).reduce((acc, senator) => acc.missedVotesPct > senator.missedVotesPct ? acc : senator)
+
+let loyalArray = []
+
+const mostLoyal = getSimplifiedSenators(republicans).reduce((acc, senator) => {
+    if (senator.loyaltyPct === 100) {
+        loyalArray.push(senator)
     }
-)
+    return acc.loyaltyPct > senator.loyaltyPct ? acc : senator
+})
 
-console.log(mostSeniority)
-populateSenatorDiv(getSimplifiedSenators(republicans))}
+console.log(loyalArray)
+
+populateSenatorDiv(getSimplifiedSenators(senators))
